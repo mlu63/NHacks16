b63ad5347dbda4612304213288d5c3b390868622
Michael Lu <michaeldelu@gmail.com>
2016-03-12 14:44:19 -0500

    testing PHP stuff

--- account/signin_1.php
+++ account/signin_1.php
@@ -1,9 +1,37 @@
+<!-- PHP to handle DB interactions and login -->
+<?php
+  $host = "ec2-52-23-196-2.compute-1.amazonaws.com";
+  $user = "teamshawerma";
+  $pass = "shawerma1";
+  $db = "gamesexchange";
+
+  mysql_connect($host, $user, $pass);
+  mysql_select_db($db);
+  
+  // check if the e-mail is set
+  if (isset($_POST['email'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM UserProfile WHERE Email='".$email."' AND Password='".$password."' LIMIT 1";
+    $response = mysql_query($sql);
+    // got a response
+    if (mysql_num_rows($response) == 1) {
+      // code here to validate login, for now echo
+      echo "Success!";
+      exit();
+    } else {
+      echo "Fail";
+      exit();
+    }
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>Sign-In Page</title>
+  <title>Authentication Page</title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -16,42 +44,36 @@
 
   <nav class="orange" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="../index.html" class="brand-logo">GC</a>
-    <!--
-      <ul class="right hide-on-med-and-down">
-        <li><a href="account/signin.html">Login</a></li>
-      </ul>
-
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="account/signin.html">Login</a></li>
-      </ul>
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-    </div>
-    -->
   </nav>
 
   <div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <br><br>
-      <h1 class="header center orange-text">Join the Community.</h1>
+      <h1 class="header center orange-text">Log In.</h1>
 
-      <!--
-      <div class="row center">
-        <h5 class="header col s12 light">A trading platform for lending games</h5>
-      </div> -->
       <br><br>
       <br><br>
-      <div class="row center">
-        <a href="" id="download-button" class="btn-large btn-rnd waves-effect waves-light orange">Sign-up with Facebook</a>
+      <div class="row">
+        <form class="col s12">
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="email" type="email" class="validate">
+              <label for="email">E-mail</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="password" type="password" class="validate">
+              <label for="password">Password</label>
+            </div>
+          </div>
+        </form>
       </div>
-      <div class="row center">
-      <a href="signup.html" id="download-button" class="btn-large btn-rnd waves-effect waves-light orange">Create an Account</a>
-
+      
       <br><br>
       <div class="row center">
-        <h7 class="header col s12 light">Already a Member?</h7>
-      </div> 
-      <div class="row center">
-      <a href="signin_1.php" id="download-button" class="btn-large btn-rnd waves-effect waves-light light-blue lighten-1">Log In</a>
+        <a href="" id="download-button" class="btn-large btn-rnd waves-effect waves-light light-blue lighten-1">Authenticate</a>
+      </div>
 
     </div>
     <br><br>
