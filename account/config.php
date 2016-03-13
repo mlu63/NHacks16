2a55e203d1db38b34115f4e4c28bbad864ffe926
Michael Lu <michaeldelu@gmail.com>
2016-03-12 20:25:34 -0500

    fuk2.1

---
+++ account/config.php
@@ -0,0 +1,8 @@
+<?php
+   define('DB_SERVER', 'gamesexchange.ckfgkbliiowl.us-east-1.rds.amazonaws.com');
+   define('DB_USERNAME', 'teamshawerma');
+   define('DB_PASSWORD', 'shawerma1');
+   define('DB_DATABASE', 'GamesExchange');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
+
